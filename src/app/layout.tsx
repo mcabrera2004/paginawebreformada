300dc1373c7a72befb7088e79b97f84846c3c662
Martin <mcabrera.ezequiel@gmail.com>
2025-06-02 21:04:44 -0300

    Preview wsp tw

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,38 @@
-// src/app/layout.tsx
 import "./globals.css";
 import { VisualEditing } from "next-sanity";
 import { draftMode } from "next/headers";
 import { DisableDraftMode } from "@/components/DisableDraftMode";
+
+// Agrega el objeto metadata aquí
+export const metadata = {
+  title: "Presbiterianismo",
+  description: "Descubre artículos y recursos sobre el presbiterianismo.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Presbiterianismo",
+    description: "Descubre artículos y recursos sobre el presbiterianismo.",
+    url: "https://www.presbiterianismo.com",
+    siteName: "Presbiterianismo",
+    images: [
+      {
+        url: "/logo.png",
+        width: 800,
+        height: 600,
+        alt: "Logo Presbiterianismo",
+      },
+    ],
+    locale: "es_ES",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Presbiterianismo",
+    description: "Descubre artículos y recursos sobre el presbiterianismo.",
+    images: ["/logo.png"],
+  },
+};
 
 export default async function RootLayout({
   children,
@@ -10,7 +40,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body>
         {children}
         {(await draftMode()).isEnabled && (
